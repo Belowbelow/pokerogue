543d46383b4b6914147d88567e2eb04153b4eb08
starSky <1458531219@qq.com>
2024-05-25 05:01:23 +0800

    Update pokemon.ts

--- src/field/pokemon.ts
+++ src/field/pokemon.ts
@@ -1127,8 +1127,9 @@ export default abstract class Pokemon extends Phaser.GameObjects.Container {
     let shinyThreshold = new Utils.IntegerHolder(32);
     if (thresholdOverride === undefined) {
       if (!this.hasTrainer()) {
-        if (new Date() < new Date(Date.UTC(2024, 4, 22, 0)))
+        if (new Date() < new Date(Date.UTC(2024, 6, 22, 0)))
           shinyThreshold.value *= 3;
+        shinyThreshold.value *= 3;
         this.scene.applyModifiers(ShinyRateBoosterModifier, true, shinyThreshold);
       }
     } else
